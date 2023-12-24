897530fbfc42d605e9d1c294c8be4be7c739e58b
denizbilgin4 <denizbilgin@posta.mu.edu.tr>
2023-12-24 18:22:37 +0300

    all students & all courses table completed. Filtering student & courses completed

--- get-all-courses.js
+++ get-all-courses.js
@@ -1,3 +1,73 @@
 export function getAllCoursesMain(coursesDb) {
+    var lenCourses = coursesDb.getCountOfAllCourses();
+    var lenCoursesSpan = document.getElementById("courses-length");
+    lenCoursesSpan.innerHTML = lenCourses;
+
+    var coursesTable = document.getElementById("courses-table");
+    setCoursesTable(coursesDb.getCoursesFromLocalStorage(), coursesTable);
+    
+
+    const mySearchButton = document.getElementById("my-course-search-button");
+    mySearchButton.addEventListener("click", function() {
+        searchByCourseName(coursesDb, coursesTable);
+    });
+
+    const myClearButton = document.getElementById("my-course-clear-button");
+    myClearButton.addEventListener("click", function() {
+        setCoursesTable(coursesDb.getCoursesFromLocalStorage(), coursesTable);
+        document.getElementById("course-search-input").value = "";
+    });
+}
+
+function setCoursesTable(courses, coursesTable, tableHeader = "<tr>" + "<th>Course ID</th>" + "<th>Course Name</th>" + "<th>Instructor</th>" + "<th>ACTS</th>" + "<th>Midterm & Final Percenty</th>" + "<th>Number of Learners</th>" + "</tr>"){
+    coursesTable.innerHTML = "";
+    coursesTable.innerHTML += tableHeader;
+    for (let i = 0; i < courses.length; i++) {
+        const course = courses[i];
+        var courseInfo =
+            "<tr>" +
+            "<td>" + course.courseId + "</td>" +
+            "<td>" + course.name + "</td>" +
+            "<td>" + course.instructor + "</td>" +
+            "<td>" + course.acts + "</td>" +
+            "<td>" + course.midtermPercent + "% - " + course.finalPercent + "%</td>" +
+            "<td>" + course.getNumberOfStudents() + "</td>" +
+            "</tr>";
+            coursesTable.innerHTML += courseInfo;
+    }
+
+    var tableRows = document.querySelectorAll("#courses-table tr");
+    for (let i = 0; i < tableRows.length; i++) {
+        if(i%2 == 0){
+            tableRows[i].style.backgroundColor = "#dddddd";
+        }
+    }
+}
+
+function capitalizeWords(input){
+    let words = input.split(' ');
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    }
+    return words.join(' ');
+}
+
+function searchByCourseName(coursesDb, coursesTable , tableHeader = "<tr>" + "<th>Course ID</th>" + "<th>Course Name</th>" + "<th>Instructor</th>" + "<th>ACTS</th>" + "<th>Midterm & Final Percenty</th>" + "<th>Number of Learners</th>" + "</tr>"){
+    coursesTable.innerHTML = "";
+    coursesTable.innerHTML += tableHeader;
+    var userInput = document.getElementById("course-search-input").value;
+    
+    if (userInput) {
+        userInput = capitalizeWords(userInput);
+        var result = coursesDb.getCourseByName(userInput);
+        if (result.length === 0) {
+            setCoursesTable(coursesDb.getCoursesFromLocalStorage(), coursesTable);
+        } else {
+            setCoursesTable(result, coursesTable);
+        }
+    }
+    else{
+        setCoursesTable(coursesDb.getCoursesFromLocalStorage(), coursesTable);
+    }
 
 }
--- get-all-students.js
+++ get-all-students.js
@@ -1,13 +1,30 @@
 
 export function getAllStudentsMain(studentsDb) {
     var lenStudents = studentsDb.getCountOfAllStudents();
-    var lenStudentsDiv = document.getElementById("students-length");
-    lenStudentsDiv.innerHTML = lenStudents;
+    var lenStudentsSpan = document.getElementById("students-length");
+    lenStudentsSpan.innerHTML = lenStudents;
 
-    // Get All Of the Students
     var studentsTable = document.getElementById("students-table");
-    for (let i = 0; i < lenStudents; i++) {
-        const student = studentsDb.getStudentsFromLocalStorage()[i];
+    setStudentsTable(studentsDb.getStudentsFromLocalStorage(), studentsTable);
+
+    const mySearchButton = document.getElementById("my-student-search-button");
+    mySearchButton.addEventListener("click", function() {
+        searchByStudentNameOrStudentId(studentsDb, studentsTable);
+    });
+
+    const myClearButton = document.getElementById("my-student-clear-button");
+    myClearButton.addEventListener("click", function(){
+        setStudentsTable(studentsDb.getStudentsFromLocalStorage(), studentsTable);
+        document.getElementById("student-search-input").value = "";
+    });
+    
+}
+
+function setStudentsTable(students, studentsTable, tableHeader = "<tr>" + "<th>Student ID</th>" + "<th>Name</th>" + "<th>Surname</th>" + "<th>GPA</th>" + "<th>Total ACTS</th>" + "</tr>") {
+    studentsTable.innerHTML = "";
+    studentsTable.innerHTML += tableHeader;
+    for (let i = 0; i < students.length; i++) {
+        const student = students[i];
         var studentInfo = 
         "<tr>" +
         "<td>" + student.studentId + "</td>" +
@@ -24,5 +41,33 @@ export function getAllStudentsMain(studentsDb) {
         if(i%2 == 0){
             tableRows[i].style.backgroundColor = "#dddddd";
         }
+    }
+}
+
+function capitalizeWords(input){
+    let words = input.split(' ');
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    }
+    return words.join(' ');
+}
+
+function searchByStudentNameOrStudentId(studentsDb, studentsTable, tableHeader = "<tr>" + "<th>Student ID</th>" + "<th>Name</th>" + "<th>Surname</th>" + "<th>GPA</th>" + "<th>Total ACTS</th>" + "</tr>") {
+    studentsTable.innerHTML = "";
+    studentsTable.innerHTML += tableHeader;
+    var result = undefined;
+    var userInput = document.getElementById("student-search-input").value;
+
+    if (userInput) {
+        userInput = capitalizeWords(userInput);
+        result = studentsDb.getStudentsByName(userInput);
+        if (result.length === 0) {
+            setStudentsTable(studentsDb.getStudentsFromLocalStorage(), studentsTable);
+        }
+        else{
+            setStudentsTable(result, studentsTable);
+        }
+    } else {
+        setStudentsTable(studentsDb.getStudentsFromLocalStorage(), studentsTable);
     }
 }
